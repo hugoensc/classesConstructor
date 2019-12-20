db83542a542226c8834646d7082d6ad0565ceee3
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2019-12-20 12:20:55 -0600

    FirstCommit

---
+++ Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classesConstructor
+{
+    class Person
+    {
+        private string firstName;
+        private int age = 28;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName { get; set; }
+        public int Age 
+        {
+            get
+            {
+                return age;
+            }
+        }
+
+        public List<string> Telefonos { get; set; }
+
+        public Person()
+        {
+            Console.WriteLine("Ensamblando humano....\n\n\t\t Humano Ensamblado ;)");
+            Telefonos = new List<string>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace classesConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person();
+            //Console.WriteLine(person1.Age);
+            person1.Telefonos.Add("7711744182");
+            Console.WriteLine(person1.Telefonos[0]);
+        }
+    }
+}
